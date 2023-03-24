4585b9b08b72265f816270729441c736c61df87c
Natalie R <tatysia@gmail.com>
2023-03-24 19:23:07 +0300

    Домашняя работа № 7

---
+++ Ex36.py
@@ -0,0 +1,13 @@
+# Задача. Функция print_operation_table(operation, num_rows=6, num_columns=6), которая принимает 
+# в качестве аргумента функцию, вычисляющую элемент по номеру строки и столбца. 
+# Аргументы num_rows и num_columns указывают число строк и столбцов таблицы,  которые должны быть
+# распечатаны. Нумерация строк и столбцов идет с единицы (подумайте, почему не с нуля).
+
+def print_operation_table(operation, num_rows, num_columns):
+    arr = [[operation(i, j) for i in range(1, num_rows + 1)] for j in range(1, num_columns + 1)]
+    for i in arr:
+        print(*[f'{x:>3}' for x in i])
+
+num_x = int(input('Введите количество строк: '))
+num_y = int(input('Введите количество столбцов: '))
+print_operation_table(lambda x, y: x + y, num_x, num_y)
